2ebe7b4300282719496bd471f77eae80b11cf7bf
jesusbeb <jesusbelb@gmail.com>
2026-01-21 19:02:08 -0600

    Propiedades ACID y Spring Data: Uso de @Transactional

--- src/main/java/com/jbeb/pizza/service/PizzaService.java
+++ src/main/java/com/jbeb/pizza/service/PizzaService.java
@@ -4,12 +4,14 @@ import com.jbeb.pizza.persistence.entity.PizzaEntity;
 import com.jbeb.pizza.persistence.repository.PizzaPagSortRepository;
 import com.jbeb.pizza.persistence.repository.PizzaRepository;
 import com.jbeb.pizza.service.dto.UpdatePizzaPriceDto;
+import com.jbeb.pizza.service.exception.EmailApiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -82,11 +84,25 @@ public class PizzaService {
         this.pizzaRepository.deleteById(idPizza);
     }
 
-    // @Transactional
-    @Transactional
+    // @Transactional permite garantizar las cuatro caracteristicas ACID. Es muy importante que siempre que se tengan
+    // dos o mas llamados a la BD desde un mismo metodo, si o si usar @Transactional
+    // Supongamos que cuando quieramos actualizar el precio de la pizza, se envie un correo al cliente para informarle el cambio de precio
+    // Hacemos que el metodo que envia el email provoque un error. De esta manera con la anotacion @Transactional se
+    // hace primero el cambio de precio, pero como el email no se envio, se hace rollback para regresar a su precio original la pizza
+    // Si a @Transactional le indicamos noRollbackFor y expecificamos la clase que maneja la excepcion, entonces no hara
+    // rollback cuando ocurra esa excepcion, pero si lo hara cuando ocurra alguna otra excepcion. Por lo tanto si se hara el cambio de precio
+    // Propagation.REQUIRED significa que debe existir una transaccion para poder ejecutar el metodo, si no existe, la creara automaticamente
+    @Transactional(noRollbackFor = EmailApiException.class,
+    propagation = Propagation.REQUIRED)
     public void updatePrice(UpdatePizzaPriceDto dto){
         this.pizzaRepository.updatePrice(dto.getPizzaId(), dto.getNewPrice());
+        this.sendEmail();
        //this.pizzaRepository.updatePrice(updatePizzaPriceDto); // se envia el dto en caso de usar el metodo que usa Spring Expression Language
+    }
+
+    // Metodo solo para lanzar una excepcion. Provoca un error intencional
+    private void sendEmail(){
+        throw new EmailApiException();
     }
 
     // Metodo para comprobar si una pizza ya existe
@@ -95,3 +111,34 @@ public class PizzaService {
     }
 
 }
+
+
+
+/*
+¿Qué es ACID y por qué es vital en las transacciones de bases de datos?
+
+Cuando trabajamos con bases de datos en nuestras aplicaciones, la integridad y confiabilidad de las transacciones son
+esenciales para asegurar que la información almacenada esté siempre precisa y segura. ACID es un conjunto de
+cuatro propiedades que toda transacción debe cumplir para garantizar su confiabilidad.
+
+- Atomicidad: Esta propiedad asegura que las transacciones sean "todo o nada". Si un error ocurre durante la ejecución,
+              se realiza un rollback para restaurar el estado inicial.
+
+- Consistencia: Valida que las transacciones solo realicen operaciones que mantienen la integridad de la
+                información. Esto garantiza que las bases de datos no se corrompan con datos inválidos.
+
+- Aislamiento: Garantiza que las transacciones se ejecuten de manera independiente, evitando que los datos se
+               mezclen entre operaciones concurrentes.
+
+- Durabilidad: Asegura que la información persista a lo largo del tiempo, incluso si la base de datos se apaga y se
+               vuelve a encender.
+
+ ¿Cómo implementa Spring Data JPA ACID con @Transactional?
+Spring Data JPA proporciona la anotación @Transactional para manejar las propiedades ACID en transacciones que
+involucran múltiples operaciones de base de datos.
+
+Al anotar un método con @Transactional, se asegura que las transacciones sean atómicas, aisladas, durables y
+consistentes.
+Es vital usar @Transactional cuando se hacen múltiples llamados a la base de datos desde un mismo método, ya que
+asegura que las operaciones se ejecuten de manera integral.
+ */
